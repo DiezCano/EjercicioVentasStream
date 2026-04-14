14923b6f3c89ba270958373ca57487173ce9f0a7
DiezCano <jdiecan2926@gmail.com>
2026-04-14 16:35:38 +0200

    EjercicioVentasStream

---
+++ src/main/java/utilidades/CsvLoades.java
@@ -0,0 +1,43 @@
+package utilidades;
+
+import entidades.Venta;
+
+import java.io.IOException;
+import java.nio.file.Files;
+import java.nio.file.Path;
+import java.nio.file.Paths;
+import java.time.LocalDate;
+import java.util.List;
+
+public class CsvLoades {
+
+    public static List<Venta> cargarVentas(String rutaFichero) {
+
+        Path ruta  = Paths.get("src/main/resources/ventas.csv");
+        List<Venta> ventas = null;
+
+        try {
+            List<String> filas = Files.readAllLines(ruta);
+           filas.forEach(l -> {
+               List<String> campos = List.of(l.split(","));
+               ventas.add(new Venta(
+                       Long.parseLong(campos.get(0)),
+                       campos.get(1),
+                       campos.get(2),
+                       campos.get(3),
+                       Integer.parseInt(campos.get(4)),
+                       Double.parseDouble(campos.get(5)),
+                       LocalDate.parse(campos.get(6)),
+                       campos.get(7)
+
+               ));
+           });
+        } catch (IOException e) {
+            throw new RuntimeException(e);
+        }
+
+
+
+        return ventas;
+    }
+}
