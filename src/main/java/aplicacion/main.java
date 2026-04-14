c5035f54e24968f9edda2f4852a46f7c1c366f21
DiezCano <jdiecan2926@gmail.com>
2026-04-14 17:17:19 +0200

    EjercicioVentasStream

--- src/main/java/aplicacion/main.java
+++ src/main/java/aplicacion/main.java
@@ -1,6 +1,7 @@
 package aplicacion;
 
 import entidades.Venta;
+import servicios.ConsultasVentas;
 import utilidades.CsvLoades;
 
 import java.util.List;
@@ -10,7 +11,26 @@ public class main {
     static void main() {
 
         List<Venta> ventas = CsvLoades.cargarVentas();
-        ventas.forEach(System.out::println);
+
+        ConsultasVentas consVentas = new ConsultasVentas(ventas);
+
+        IO.println();
+        // --- 1 ---
+        consVentas.getVentasMayor100()
+                .forEach(System.out::println);
+        // --- 2 ---
+        consVentas.getVentasByCategoriaElectronica()
+                .forEach(System.out::println);
+        // --- 3 ---
+        consVentas.getProductosVendidosOrder()
+                .forEach(System.out::println);
+        // --- 4 ---
+        consVentas.getPrimeraVentaSpain()
+                .ifPresent(System.out::println);
+        // --- 5 ---
+        consVentas.get10VentasImporteTotal()
+                .forEach(System.out::println);
+        // --- 6 ---
 
     }
 }
---
+++ src/main/java/servicios/ConsultasVentas.java
@@ -0,0 +1,92 @@
+package servicios;
+
+import entidades.Venta;
+import lombok.Data;
+
+import java.util.*;
+import java.util.stream.Collectors;
+
+@Data
+public class ConsultasVentas {
+
+    private List<Venta> ventas;
+
+    public ConsultasVentas(List<Venta> ventas) {
+        this.ventas = ventas;
+    }
+
+    //1. Ventas con importe total > 100€
+    public List<Venta> getVentasMayor100() {
+        return ventas.stream()
+                .filter(venta -> venta.getTotalLinea()> 100)
+                .toList();
+    }
+
+    //2. Ventas de la categoría "Electrónica"
+    public List<Venta> getVentasByCategoriaElectronica() {
+        return ventas.stream()
+                .filter(v ->v.getCategoria().equals("Electronics"))
+                .toList();
+    }
+    //3. Productos distintos vendidos
+    public List<String> getProductosVendidosOrder() {
+        return ventas.stream()
+                .map(Venta::getProducto)
+                .distinct()
+                .sorted()
+                .toList();
+    }
+    //4. Primera venta registrada de España
+    public Optional<Venta> getPrimeraVentaSpain() {
+        return ventas.stream()
+                .filter(v -> v.getPais().equals("Spain"))
+                .findFirst();
+    }
+    //5. Top 10 ventas por importe total
+    public List<Venta> get10VentasImporteTotal() {
+        return ventas.stream()
+                .sorted(Comparator.comparing(Venta::getTotalLinea).reversed())
+                .limit(10)
+                .toList();
+    }
+    //6. Facturación total
+    public double getFacturacionTotal() {
+        return ventas.stream()
+                .mapToDouble(Venta::getTotalLinea)
+                .sum();
+    }
+    //7. Estadísticas del precio unitario
+    public DoubleSummaryStatistics getEstadisticasPrecioUnitario() {
+        return ventas.stream()
+                .mapToDouble(Venta::getPrecioUnitario)
+                .summaryStatistics();
+    }
+    //8. Número de ventas por categoría
+    public Map<String, Long> getNUmeroDeVentasPorCategoria() {
+        return ventas.stream()
+                .collect(Collectors.groupingBy(Venta::getCategoria,
+                        Collectors.counting()));
+    }
+    //9. Facturación total por país
+    public Map<String, Double> getFacturacionTotalPorPais() {
+        return ventas.stream()
+                .collect(Collectors.groupingBy(Venta::getPais,
+                        Collectors.summingDouble(Venta::getTotalLinea)));
+    }
+    //10. Número de ventas por método de pago
+    public Map<String, Long> getNUmeroDeVentasPorMetodoDePago() {
+        return ventas.stream()
+                .collect(Collectors.groupingBy(Venta::getMetodoPago,
+                        Collectors.counting()));
+    }
+    //11. Facturación por categoría
+
+    //12. Ventas agrupadas por mes (1–12)
+
+    //13. Categoría con más unidades vendidas
+
+    //14. ¿Todas las ventas con BIZUM son < 200€?
+
+    //15. Porcentaje de ventas pagadas con TARJETA
+
+}
